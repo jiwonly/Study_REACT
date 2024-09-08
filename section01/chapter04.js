a66d0102d72fa227e7285934756d98c8584be46a
Jun Jiwon <ysjjw2003@naver.com>
2024-09-08 19:02:12 +0900

    add chapter 3~7

---
+++ section01/chapter04.js
@@ -0,0 +1,17 @@
+// 1. 변수
+let age = 27;
+
+age = 30;
+
+// 2. 상수 (변하면 X, 초기화 필수)
+const birth = "2003.12.22";
+
+// 3. 변수 명명규칙(네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용할 수 없다.
+// 3-2. 숫자로 시작할 수 없다.
+// 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
