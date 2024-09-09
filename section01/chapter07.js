24c258918902d9c7c36f4828a20aad39b85d6967
Jun Jiwon <ysjjw2003@naver.com>
2024-09-09 23:38:58 +0900

    fix ch07

--- section01/chapter07.js
+++ section01/chapter07.js
@@ -7,3 +7,30 @@ let num2 = 3 - 2;
 let num3 = 3 * 2;
 let num4 = 3 / 2;
 let num5 = 3 % 2;
+
+// 3. 복합 대입 연산자
+let num7 = 10;
+num7 += 20;
+
+// 4. 증감 연산자
+let num8 = 10;
+num8++;
+++num8;
+
+// 5. 논리 연산자
+let or = true || false;
+let and = true && false;
+let not = !true;
+
+// 6. 비교 연산자
+let comp1 = 1 === 2;
+// ===는 자료형까지 비교!
+// ==는 자료형이 달라도 값이 같으면 같다고 판단
+// ex) 1=="1"를 true로 출력
+let comp2 = 1 !== 2;
+
+let comp3 = 2 > 1;
+let comp4 = 2 < 1;
+
+let comp5 = 2 >= 2;
+let comp6 = 2 <= 2;
