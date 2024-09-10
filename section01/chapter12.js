165c72e7d2e3560869f68c824e95e528ff3743c0
Jun Jiwon <ysjjw2003@naver.com>
2024-09-10 23:38:32 +0900

    add 함수 표현식&화살표 함수

---
+++ section01/chapter12.js
@@ -0,0 +1,23 @@
+// 1. 함수 표현식 (호이스팅 X)
+
+function funcA() {
+  //   console.log("funcA");
+}
+
+let varA = funcA;
+varA(); // funcA가 그대로 출력
+
+let varB = function () {
+  // 익명함수 (이름이 없다)
+  //   console.log("funcB");
+};
+
+varB(); // funcB 출력
+
+// 2. 화살표 함수
+let varC = (value) => {
+  console.log(value);
+  return value + 1;
+};
+
+console.log(varC(3));
