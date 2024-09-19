4deced894ff3ee0631939036e22e4c321005658a
Jun Jiwon <ysjjw2003@naver.com>
2024-09-19 14:29:15 +0900

    ADD lifecycle

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,10 +1,38 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
+  useEffect(() => {
+    console.log("mount");
+  }, []); // 처음 이후에 호츌되지 X
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
+
+  // useEffect(() => {
+  //   console.log(`count: ${count} / input: ${input}`);
+  // }, [count, input]);
+  // // 두번째 값이 바뀔 때마다 첫번째 값이 실행됨
+  // // 두번째 배열을 의존성 배열이라고 부름
+  // // dependency array
+  // // deps
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -14,7 +42,16 @@ function App() {
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+      <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
 
       <section>
