d60d12681aa9f389bbf12defdabc219a80a1b68f
Jun Jiwon <ysjjw2003@naver.com>
2024-09-19 13:57:22 +0900

    ADD section06

---
+++ section06/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { useState } from "react";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }; // 이벤트 핸들러
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
